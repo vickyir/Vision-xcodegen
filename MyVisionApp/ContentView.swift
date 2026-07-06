e29fae4cea75ddaa9500948c77854ba1fe4dcbaa
vickyir <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>
2026-07-06 14:28:20 +0700

    Initial Commit

---
+++ MyVisionApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyVisionApp
+//
+//  Created by Guest 2026 on 06/07/26.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ MyVisionApp/MyVisionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVisionAppApp.swift
+//  MyVisionApp
+//
+//  Created by Guest 2026 on 06/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MyVisionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
